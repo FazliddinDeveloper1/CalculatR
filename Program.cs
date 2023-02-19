e0f184245cb9406ce015ef1d8d3f975f0362ccf1
FazliddinDeveloper1 <fazliddinx0506@gmail.com>
2023-02-19 16:01:36 +0500

    Project CalculatR(1st commit)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Adding: {firstNumber + secondNumber}");
+Console.WriteLine($"Substracting: {firstNumber - secondNumber}");
+Console.WriteLine($"Dividing: {firstNumber / secondNumber}");
+Console.WriteLine($"Multiplying: {firstNumber * secondNumber}");
+
+Console.WriteLine($"Qoldiq {firstNumber % secondNumber}");
